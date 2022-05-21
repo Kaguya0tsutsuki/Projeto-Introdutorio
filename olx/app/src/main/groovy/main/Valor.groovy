8b3f27ba41125dc089db92922494bc17bbdabb53
Islaine Alves <islaine.a.c@gmail.com>
2022-05-21 15:04:20 -0300

    revert

---
+++ olx/app/src/main/groovy/main/Valor.groovy
@@ -0,0 +1,15 @@
+package main
+
+class Valor {
+    static BigDecimal mediaValor(List<OlxClass> list){
+
+        BigDecimal average = 0.00
+
+        for (OlxClass data: list) {
+            average = (average + data.preco)/2
+        }
+
+        return average
+
+    }
+}
